a0d4b54673bc26c03dac262ae7d299b52a823720
Sami Haddad <samihaddad13@gmail.com>
2020-06-27 14:49:43 +0300

    Add get, x64hash128 functions

--- lib/fingerprintjs.dart
+++ lib/fingerprintjs.dart
@@ -4,29 +4,37 @@ library fingerprintjs;
 import 'dart:async';
 import 'package:js/js.dart';
 
+part 'src/options.dart';
+
 @JS('Fingerprint2')
 class _Fingerprint {
-  external static dynamic get(Function action);
+  external static dynamic get(Options options, Function action);
   external static dynamic x64hash128(String values, int seed);
 }
 
 @JS()
 class FingerprintComponent {
-  external dynamic get value;
   external String get key;
+  external dynamic get value;
 }
 
 class Fingerprint {
-  static Future<String> getHash() {
-    final completer = Completer<String>();
+  static Future<List<FingerprintComponent>> get({Options options}) {
+    final completer = Completer<List<FingerprintComponent>>();
     Timer(Duration(milliseconds: 500), () {
-      _Fingerprint.get(allowInterop((components) {
-        List values =
-            components.map((component) => component.value.toString()).toList();
-        String _hash = _Fingerprint.x64hash128(values.join(''), 31);
-        completer.complete(_hash);
+      _Fingerprint.get(options, allowInterop((components) {
+        completer.complete(components.cast<FingerprintComponent>());
       }));
     });
     return completer.future;
+  }
+
+  static String x64hash128(String key, int seed) {
+    return _Fingerprint.x64hash128(key, seed);
+  }
+
+  static Future<String> getHash() async {
+    var values = await Fingerprint.get();
+    return Fingerprint.x64hash128(values.join(''), 31);
   }
 }
---
+++ lib/src/options.dart
@@ -0,0 +1,67 @@
+part of fingerprintjs;
+
+@JS()
+@anonymous
+class FontOptions {
+  external String get swfContainerId;
+  external String get swfPath;
+  external List<String> get userDefinedFonts;
+  external bool get extendedJsFonts;
+
+  external factory FontOptions({
+    String swfContainerId,
+    String swfPath,
+    List<String> userDefinedFonts,
+    bool extendedJsFonts,
+  });
+}
+
+@JS()
+@anonymous
+class ScreenOptions {
+  external bool get detectScreenOrientation;
+
+  external factory ScreenOptions({bool detectScreenOrientation});
+}
+
+@JS()
+@anonymous
+class AudioOptions {
+  external num get timeout;
+  external bool get excludeIOS11;
+
+  external factory AudioOptions({num timeout, bool excludeIOS11});
+}
+
+
+
+@JS()
+@anonymous
+class PluginOptions {
+  external List get sortPluginsFor;
+  external bool get excludeIE;
+
+  external factory PluginOptions({List sortPluginsFor, bool excludeIE});
+}
+
+@JS()
+@anonymous
+class Options {
+  external Function get preprocessor;
+  external AudioOptions get audio;
+  external FontOptions get fonts;
+  external ScreenOptions get screen;
+  external PluginOptions get plugin;
+  external List get extraComponents;
+
+
+
+  external factory Options({
+    Function preprocessor,
+    AudioOptions audio,
+    FontOptions fonts,
+    ScreenOptions screen,
+    PluginOptions plugins,
+    List extraComponents,
+  });
+}
